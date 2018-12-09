43ec44891de35318cdb7c6ece865e0b47386c6ce
LiangOrdinary <1415192242@qq.com>
2018-12-09 21:12:20 +0800

    进行开发

---
+++ app/src/main/java/com/test/lc/myapplication/activity/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.test.lc.myapplication.activity;
+
+import com.test.lc.myapplication.R;
+import com.test.lc.myapplication.base.BaseActivity;
+
+/**
+ * Created by 梁矗 on 2018/12/9.
+ */
+
+public class HomeActivity extends BaseActivity {
+    @Override
+    protected void initView() {
+
+
+
+    }
+
+    @Override
+    protected int getActivityLayoutId() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
--- app/src/main/java/com/test/lc/myapplication/activity/WelComeActivity.java
+++ app/src/main/java/com/test/lc/myapplication/activity/WelComeActivity.java
@@ -1,16 +1,37 @@
 package com.test.lc.myapplication.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.test.lc.myapplication.R;
 import com.test.lc.myapplication.base.BaseActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class WelComeActivity extends BaseActivity {
 
 
+    private ImageView welcome;
+
     @Override
     protected void initView() {
+
+        welcome = findViewById(R.id.welcome);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(WelComeActivity.this, HomeActivity.class));
+
+                finish();
+
+            }
+        }, 3000);
 
     }
 
