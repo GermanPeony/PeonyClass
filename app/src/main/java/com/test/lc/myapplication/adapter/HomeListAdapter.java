75c349b07db159d4c2cc86d1673e7bc0eb6b6ae3
LiangOrdinary <1415192242@qq.com>
2018-12-11 00:17:32 +0800

    首页布局的搭建

---
+++ app/src/main/java/com/test/lc/myapplication/adapter/HomeListAdapter.java
@@ -0,0 +1,11 @@
+package com.test.lc.myapplication.adapter;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+
+/**
+ * Created by 梁矗 on 2018/12/11.
+ */
+
+public class HomeListAdapter {
+}
